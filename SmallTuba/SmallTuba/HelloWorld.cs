daeea1adb80611c7883c667e8d7775cb4b2be8bc
unknown <Christian@Christian-THINK.(none)>
2011-12-05 11:37:21 +0100

    Hello world

---
+++ SmallTuba/SmallTuba/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="HelloWorld.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace SmallTuba
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class HelloWorld
+    {
+    }
+}
