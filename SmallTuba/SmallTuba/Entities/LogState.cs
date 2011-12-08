fb6b449ef396780e9171c1f7d398b58fbeae372c
Henrik Haugbølle <henrikhaugboelle@live.dk>
2011-12-08 13:24:08 +0100

    server test suite and comments, plus a little personstate/person adding

--- SmallTuba/SmallTuba/Entities/LogState.cs
+++ SmallTuba/SmallTuba/Entities/LogState.cs
@@ -1,31 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace SmallTuba.Entities
 {
-    public class LogState
-    {
-        private PersonState voter;
+	public class LogState
+	{
+		private PersonState voter;
 
-        private string action;
+		private string action;
 
-        public LogState(PersonState voter, string action)
-        {
-            this.voter = voter;
-            this.action = action;
-        }
+		public LogState(PersonState voter, string action)
+		{
+			this.voter = voter;
+			this.action = action;
+		}
 
-        public PersonState Voter
-        {
-            get { return voter; }
-        }
+		public PersonState Voter
+		{
+			get { return voter; }
+		}
 
-        public override string ToString()
-        {
-            return voter.Time.ToLocalTime().Hour + ":" + voter.Time.ToLocalTime().Minute + " " + voter.FirstName + " " +
-                   voter.LastName + " was " + action;
-        }
-    }
+		public override string ToString()
+		{
+			// Needs the TimeConverter class to uncomment this
+		   //  return voter.Time.ToLocalTime().Hour + ":" + voter.Time.ToLocalTime().Minute + " " + voter.FirstName + " " +
+			 //      voter.LastName + " was " + action;
+
+			return "";
+		}
+	}
 }
