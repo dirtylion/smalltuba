675ecc56d905656fbafd42f6746c2897fb9c53d1
kaaresylow <kaaresylow@gmail.com>
2011-12-09 19:37:44 +0100

    xml schema checker is implemented

---
+++ SmallTuba/AdminApplication/VoterIdGenerator.cs
@@ -0,0 +1,37 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Unique_Voter_Id_Generator.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace AdminApplication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Diagnostics.Contracts;
+
+    using SmallTuba.Utility;
+
+    /// <summary>
+    /// Generates a unique id number
+    /// </summary>
+    public class VoterIdGenerator
+    {
+        private static int prevId;
+        private static int id;
+
+        public static int CreateVoterId()
+        {
+            id = TimeConverter.ConvertToUnixTimestamp(DateTime.Now);
+            while (id <= prevId)
+            {
+                id++;
+            }
+            prevId = id;
+            return id;
+        }
+
+    }
+}
