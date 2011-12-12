2b03bbd34ee230b07143d393ec500d82d95cc39b
Henrik Haugbølle <henrikhaugboelle@live.dk>
2011-12-12 11:02:48 +0100

    debug writeline

--- SmallTuba/SmallTuba/Utility/Debug.cs
+++ SmallTuba/SmallTuba/Utility/Debug.cs
@@ -1,21 +1,30 @@
 ﻿namespace SmallTuba.Utility {
+	using System;
+
 	/// <author>Henrik Haugbølle (hhau@itu.dk)</author>
-	/// <version>2011-12-08</version>
+	/// <version>2011-12-12</version>
 	/// <summary>
 	/// Static debug class used to determine whether
 	/// the project is currenctly in debug-mode.
 	/// 
 	/// Used to modify the calls to external data
 	/// sources for correct unit-test response
-	/// information.
+	/// information and to decide whether to output
+	/// debug information such as exceptions.
 	/// </summary>
 	public class Debug {
 		// Used to determine whether to use test versions of
 		// the external data sources.
-		public static bool ExternalDataSources = false;
+		public static bool ExternalDataSources;
 
 		// Used to determine whether console output should be
 		// turned on or not.
-		public static bool ConsoleOutput = false;
+		public static bool ConsoleOutput = true;
+
+		public static void WriteLine(string output) {
+			if (ConsoleOutput) { 
+				Console.WriteLine(output);
+			}
+		}
 	}
 }
